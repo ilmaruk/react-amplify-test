bab0577090be1049b898e6abe4a1cb40380b3c3c
ilmaruk-jt <ruggero.ferretti@jumptech.co.uk>
2022-01-11 15:11:59 +0100

    With env variable.

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, <code>World!</code>.
-        </p>
-      </header>
-    </div>
-  );
+    const foo = process.env.FOO ?? 'no foo, no joy!';
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Hello, <code>World</code>!
+                </p>
+            </header>
+            <footer>
+                <p>
+                    Foo is: <strong>{foo}</strong>
+                </p>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
